7df1f4ec6f88940b1b328b768c5ce8e0f4979f42
SashaGordin <55962431+SashaGordin@users.noreply.github.com>
2023-06-02 10:08:21 -0700

    trying to stress test with Client vs. Pool

--- server/db/index.js
+++ server/db/index.js
@@ -1,15 +1,36 @@
-const { Pool } = require('pg');
+// const { Pool } = require('pg');
+// require('dotenv').config();
+
+// console.log(process.env.DB_USER);
+
+// const pool = new Pool({
+//   host: process.env.HOST,
+//   database: 'products',
+//   user: process.env.DB_USER,
+//   password: process.env.PASSWORD,
+// });
+// pool.connect()
+//   .then(() => {
+//     console.log('connected');
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+
+// module.exports = pool;
+
+const { Client } = require('pg');
 require('dotenv').config();
 
 console.log(process.env.DB_USER);
 
-const pool = new Pool({
+const client = new Client({
   host: process.env.HOST,
   database: 'products',
   user: process.env.DB_USER,
   password: process.env.PASSWORD,
 });
-pool.connect()
+client.connect()
   .then(() => {
     console.log('connected');
   })
@@ -17,4 +38,4 @@ pool.connect()
     console.log(err);
   });
 
-module.exports = pool;
+module.exports = client;
