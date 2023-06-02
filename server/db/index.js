4f6945098cbc4764fce792471c54efb2b500193b
SashaGordin <55962431+SashaGordin@users.noreply.github.com>
2023-06-02 10:14:54 -0700

    Pool ended up being faster so I am keeping it

--- server/db/index.js
+++ server/db/index.js
@@ -1,36 +1,15 @@
-// const { Pool } = require('pg');
-// require('dotenv').config();
-
-// console.log(process.env.DB_USER);
-
-// const pool = new Pool({
-//   host: process.env.HOST,
-//   database: 'products',
-//   user: process.env.DB_USER,
-//   password: process.env.PASSWORD,
-// });
-// pool.connect()
-//   .then(() => {
-//     console.log('connected');
-//   })
-//   .catch((err) => {
-//     console.log(err);
-//   });
-
-// module.exports = pool;
-
-const { Client } = require('pg');
+const { Pool } = require('pg');
 require('dotenv').config();
 
 console.log(process.env.DB_USER);
 
-const client = new Client({
+const pool = new Pool({
   host: process.env.HOST,
   database: 'products',
   user: process.env.DB_USER,
   password: process.env.PASSWORD,
 });
-client.connect()
+pool.connect()
   .then(() => {
     console.log('connected');
   })
@@ -38,4 +17,25 @@ client.connect()
     console.log(err);
   });
 
-module.exports = client;
+module.exports = pool;
+
+// const { Client } = require('pg');
+// require('dotenv').config();
+
+// console.log(process.env.DB_USER);
+
+// const client = new Client({
+//   host: process.env.HOST,
+//   database: 'products',
+//   user: process.env.DB_USER,
+//   password: process.env.PASSWORD,
+// });
+// client.connect()
+//   .then(() => {
+//     console.log('connected');
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+
+// module.exports = client;
