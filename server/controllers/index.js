93cee24ace710b4c9bdbc571620a403d0e66b8ba
SashaGordin <55962431+SashaGordin@users.noreply.github.com>
2023-06-03 10:43:43 -0700

    taking out redis to use nginx laod balancer instead

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,102 +1,159 @@
+// const express = require('express');
+// const Redis = require('ioredis');
+// const models = require('../models');
+
+// const router = express.Router();
+// const redis = new Redis();
+
+// router.get('/', (req, res) => {
+//   const page = req.body.page || 1;
+//   const count = req.body.count || 5;
+//   redis.get(`${page}_${count}`)
+//     .then((result) => {
+//       if (result) {
+//         res.status(200).send(JSON.parse(result));
+//       } else {
+//         models.getProducts(page, count, (err, data) => {
+//           if (err) {
+//             console.log(err);
+//             res.status(400).send(err);
+//           } else {
+//             res.status(200).send(data.rows);
+//             redis.set(`${page}_${count}`, JSON.stringify(data.rows));
+//           }
+//         });
+//       }
+//     })
+//     .catch((err) => {
+//       res.status(400).send(err);
+//     });
+// });
+
+// router.get('/:id', (req, res) => {
+//   redis.get(req.params.id)
+//     .then((result) => {
+//       if (result) {
+//         res.status(200).send(JSON.parse(result));
+//       } else {
+//         models.getProduct(req.params.id, (err, data) => {
+//           if (err) {
+//             console.log(err);
+//             res.status(400).send(err);
+//           } else {
+//             res.status(200).send(data.rows);
+//             redis.set(req.params.id, JSON.stringify(data.rows));
+//           }
+//         });
+//       }
+//     })
+//     .catch((err) => {
+//       res.status(400).send(err);
+//     });
+// });
+
+// router.get('/:id/styles', (req, res) => {
+//   redis.get(`styles_${req.params.id}`)
+//     .then((result) => {
+//       if (result) {
+//         res.status(200).send(JSON.parse(result));
+//       } else {
+//         models.getStyles(req.params.id, (err, data) => {
+//           if (err) {
+//             console.log(err);
+//             res.status(400).send(err);
+//           } else {
+//             const ans = {
+//               product_id: req.params.id,
+//               results: data.rows,
+//             };
+//             res.status(200).send(ans);
+//             redis.set(`styles_${req.params.id}`, JSON.stringify(ans));
+//           }
+//         });
+//       }
+//     })
+//     .catch((err) => {
+//       res.status(400).send(err);
+//     });
+// });
+
+// router.get('/:id/related', (req, res) => {
+//   redis.get(`related_${req.params.id}`)
+//     .then((result) => {
+//       if (result) {
+//         res.status(200).send(JSON.parse(result));
+//       } else {
+//         models.getRelated(req.params.id, (err, data) => {
+//           if (err) {
+//             console.log(err);
+//             res.status(400).send(err);
+//           } else {
+//             res.status(200).send(data.rows[0].related);
+//             redis.set(`related_${req.params.id}`, JSON.stringify(data.rows[0].related));
+//           }
+//         });
+//       }
+//     })
+//     .catch((err) => {
+//       res.status(400).send(err);
+//     });
+// });
+
+// module.exports = router;
+
 const express = require('express');
-const Redis = require('ioredis');
 const models = require('../models');
 
 const router = express.Router();
-const redis = new Redis();
 
 router.get('/', (req, res) => {
   const page = req.body.page || 1;
   const count = req.body.count || 5;
-  redis.get(`${page}_${count}`)
-    .then((result) => {
-      if (result) {
-        res.status(200).send(JSON.parse(result));
-      } else {
-        models.getProducts(page, count, (err, data) => {
-          if (err) {
-            console.log(err);
-            res.status(400).send(err);
-          } else {
-            res.status(200).send(data.rows);
-            redis.set(`${page}_${count}`, JSON.stringify(data.rows));
-          }
-        });
-      }
-    })
-    .catch((err) => {
+  models.getProducts(page, count, (err, data) => {
+    if (err) {
+      console.log(err);
       res.status(400).send(err);
-    });
+    } else {
+      res.status(200).send(data.rows);
+    }
+  });
 });
 
 router.get('/:id', (req, res) => {
-  redis.get(req.params.id)
-    .then((result) => {
-      if (result) {
-        res.status(200).send(JSON.parse(result));
-      } else {
-        models.getProduct(req.params.id, (err, data) => {
-          if (err) {
-            console.log(err);
-            res.status(400).send(err);
-          } else {
-            res.status(200).send(data.rows);
-            redis.set(req.params.id, JSON.stringify(data.rows));
-          }
-        });
-      }
-    })
-    .catch((err) => {
+  models.getProduct(req.params.id, (err, data) => {
+    if (err) {
+      console.log(err);
       res.status(400).send(err);
-    });
+    } else {
+      res.status(200).send(data.rows);
+    }
+  });
 });
 
 router.get('/:id/styles', (req, res) => {
-  redis.get(`styles_${req.params.id}`)
-    .then((result) => {
-      if (result) {
-        res.status(200).send(JSON.parse(result));
-      } else {
-        models.getStyles(req.params.id, (err, data) => {
-          if (err) {
-            console.log(err);
-            res.status(400).send(err);
-          } else {
-            const ans = {
-              product_id: req.params.id,
-              results: data.rows,
-            };
-            res.status(200).send(ans);
-            redis.set(`styles_${req.params.id}`, JSON.stringify(ans));
-          }
-        });
-      }
-    })
-    .catch((err) => {
+  models.getStyles(req.params.id, (err, data) => {
+    if (err) {
+      console.log(err);
       res.status(400).send(err);
-    });
+    } else {
+      const ans = {
+        product_id: req.params.id,
+        results: data.rows,
+      };
+      res.status(200).send(ans);
+    }
+  });
 });
 
 router.get('/:id/related', (req, res) => {
-  redis.get(`related_${req.params.id}`)
-    .then((result) => {
-      if (result) {
-        res.status(200).send(JSON.parse(result));
-      } else {
-        models.getRelated(req.params.id, (err, data) => {
-          if (err) {
-            console.log(err);
-            res.status(400).send(err);
-          } else {
-            res.status(200).send(data.rows[0].related);
-            redis.set(`related_${req.params.id}`, JSON.stringify(data.rows[0].related));
-          }
-        });
-      }
-    })
-    .catch((err) => {
+  models.getRelated(req.params.id, (err, data) => {
+    if (err) {
+      console.log(err);
       res.status(400).send(err);
-    });
+    } else {
+      res.status(200).send(data.rows[0].related);
+    }
+  });
 });
 
 module.exports = router;
